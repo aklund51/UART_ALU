5c1d89d9ee9c407feb037f39be47dbb86223efae
aklund <aklund@ucsc.edu>
2025-01-07 12:42:47 -0800

    outline

---
+++ rtl/top.sv
@@ -0,0 +1,40 @@
+module top (
+    ports
+);
+
+
+    uart_rx
+    uart_rx_inst
+    (
+    input  wire                   clk,
+    input  wire                   rst,
+
+    /*
+     * AXI output
+     */
+    output wire [DATA_WIDTH-1:0]  m_axis_tdata,
+    output wire                   m_axis_tvalid,
+    input  wire                   m_axis_tready,
+
+    /*
+     * UART interface
+     */
+    input  wire                   rxd,
+
+    /*
+     * Status
+     */
+    output wire                   busy,
+    output wire                   overrun_error,
+    output wire                   frame_error,
+
+    /*
+     * Configuration
+     */
+    input  wire [15:0]            prescale
+
+);
+
+
+    
+endmodule
