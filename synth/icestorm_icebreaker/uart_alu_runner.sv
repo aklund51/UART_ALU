4558abd9e3db1b0ee8b1ce3b159d450f35a7ffa4
aklund <aklund@ucsc.edu>
2025-01-10 10:32:19 -0800

    comment out unused icebreaker module sim for gls to work

--- synth/icestorm_icebreaker/uart_alu_runner.sv
+++ synth/icestorm_icebreaker/uart_alu_runner.sv
@@ -15,17 +15,17 @@ initial begin
     end
 end
 
-logic pll_out;
-initial begin
-    pll_out = 0;
-    forever begin
-        #50.000ns; // 20MHz
-        pll_out = !pll_out;
-    end
-end
-assign icebreaker.pll.PLLOUTCORE = pll_out;
+// logic pll_out;
+// initial begin
+//     pll_out = 0;
+//     forever begin
+//         #50.000ns; // 20MHz
+//         pll_out = !pll_out;
+//     end
+// end
+// assign icebreaker.pll.PLLOUTCORE = pll_out;
 
-icebreaker icebreaker (.*);
+// icebreaker icebreaker (.*);
 
 // always @(posedge led) $info("Led on");
 // always @(negedge led) $info("Led off");
