6f21a54ad7f1257634781708758e3bee498f8f2b
aklund <aklund@ucsc.edu>
2025-01-09 16:21:15 -0800

    uart_alu restart btn sync

--- rtl/uart_alu.sv
+++ rtl/uart_alu.sv
@@ -1,6 +1,11 @@
 module uart_alu (
-    input clk_12mhz_i,
+    input clk_i,
     input reset_unsafe_i
 );
+    // reset to be used in synchronous design
+    logic [0:0] reset_l;
+    always_ff @(posedge clk_i) begin 
+        reset_l <= reset_unsafe_i;
+    end
     
 endmodule
