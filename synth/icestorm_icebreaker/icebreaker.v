d47137e70fe74b6035074b477f48dfd1b7e8729d
aklund <aklund@ucsc.edu>
2025-01-14 02:25:52 -0800

    transmits to connected device. wonky ready/valid signals

--- synth/icestorm_icebreaker/icebreaker.v
+++ synth/icestorm_icebreaker/icebreaker.v
@@ -3,7 +3,8 @@ module icebreaker (
     input  wire clk_i,
     input  wire reset_unsafe_i,
     input  wire RX_i,
-    output wire TX_o
+    output wire TX_o,
+    output wire LEDG_N
 );
 
 
@@ -32,13 +33,30 @@ module icebreaker (
 //     .led_o(led)
 // );
 
+wire clk_12 = clk_i;
+wire clk_48;
 
+// icepll -i 12 -o 31.5
+SB_PLL40_PAD #(
+    .FEEDBACK_PATH("SIMPLE"),
+    .DIVR(4'd0),
+    .DIVF(7'd83),
+    .DIVQ(3'd5),
+    .FILTER_RANGE(3'd1)
+) pll (
+    .LOCK(),
+    .RESETB(1'b1),
+    .BYPASS(1'b0),
+    .PACKAGEPIN(clk_12),
+    .PLLOUTCORE(clk_48)
+);
 
 uart_alu 
 uart_alu_inst(
-    .clk_i(clk_i),
+    .clk_i(clk_48),
     .reset_i(reset_unsafe_i),
     .RX_i(RX_i),
-    .TX_o(TX_o));
+    .TX_o(TX_o),
+    .LEDG_N(LEDG_N));
 
 endmodule
